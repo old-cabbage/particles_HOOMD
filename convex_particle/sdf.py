2fc090fc5c35ad8028279d07130708a8f347ed2e
OldCabbage <64997529+old-cabbage@users.noreply.github.com>
2025-03-17 21:25:17 +0800

    Add files via upload

--- convex_particle/sdf.py
+++ convex_particle/sdf.py
@@ -10,50 +10,50 @@ from multiprocessing import Pool, cpu_count
 import random
 
 def task(n):
-        #粒子总数
-        N=5000
-        num_particles = 5000
-        #每次开始插入之前，先对基础的系统预处理pre_random步数
-        pre_random = 5000
-        #粒子的总面积/盒子面积为packing_density
-        packing_density_0=0.3
-        packing_density = packing_density_0 * num_particles/N
-        #压缩系数
-        condensed_ratio=0.98
-        #微小间距
-        margin=0.2
-        
-        divice=hoomd.device.CPU()
-        mc = hoomd.hpmc.integrate.ConvexPolygon(default_d=3,default_a=0.5)
-        mc.shape["A"] = dict(
-                        vertices = [
-                        (-2, 0),
-                        (2, 0),
-                        (11/8, 5*math.sqrt(63)/8),
-                        ]
-            )
-        particle_area=5*math.sqrt(63)/4
+    #粒子总数
+    N=5000
+    num_particles = 5000
+    #每次开始插入之前，先对基础的系统预处理pre_random步数
+    pre_random = 5000
+    #粒子的总面积/盒子面积为packing_density
+    packing_density_0=0.3
+    packing_density = packing_density_0 * num_particles/N
+    #压缩系数
+    condensed_ratio=0.98
+    #微小间距
+    margin=0.2
+    
+    divice=hoomd.device.CPU()
+    mc = hoomd.hpmc.integrate.ConvexPolygon(default_d=3,default_a=0.5)
+    mc.shape["A"] = dict(
+                    vertices = [
+                    (-2, 0),
+                    (2, 0),
+                    (11/8, 5*math.sqrt(63)/8),
+                    ]
+        )
+    particle_area=5*math.sqrt(63)/4
 
-        system=System(
-                num=num_particles,packing_density=packing_density,
-                packing_density_0=packing_density_0,
-                particle_area=particle_area,
-                mc=mc,condensed_ratio=condensed_ratio,margin=margin,
-                pre_random=pre_random,device=divice
-            )
+    system=System(
+            num=num_particles,packing_density=packing_density,
+            packing_density_0=packing_density_0,
+            particle_area=particle_area,
+            mc=mc,condensed_ratio=condensed_ratio,margin=margin,
+            pre_random=pre_random,device=divice
+        )
 
-        system.simulation = hoomd.Simulation(device=divice,seed=random.randint(1,10+n))
-        system.simulation.operations.integrator = mc
-        system.simulation.create_state_from_gsd(filename='./gsd/P_{:.2f}_{}.gsd'.format(packing_density_0,num_particles))
+    system.simulation = hoomd.Simulation(device=divice,seed=random.randint(1,10+n))
+    system.simulation.operations.integrator = mc
+    system.simulation.create_state_from_gsd(filename='./gsd/P_{:.2f}_{}.gsd'.format(packing_density_0,num_particles))
 
-        sdf_mc=1000
-        sdf_xmax=0.01
-        sdf_dx=1e-4
-        sdf_each_run=5
+    sdf_mc=1000
+    sdf_xmax=0.01
+    sdf_dx=1e-4
+    sdf_each_run=5
 
-        total_sdf_xcompression,total_sdf_sdfcompression=system.calculate_sdf(sdf_mc,sdf_xmax,sdf_dx,sdf_each_run)
+    total_sdf_xcompression,total_sdf_sdfcompression=system.calculate_sdf(sdf_mc,sdf_xmax,sdf_dx,sdf_each_run)
 
-        return total_sdf_xcompression,total_sdf_sdfcompression
+    return total_sdf_xcompression,total_sdf_sdfcompression
 
 def main():
 
